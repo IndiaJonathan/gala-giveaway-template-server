abf09d7eaa01853789580f125c716b48dd8f798a
Jonathan B <jburk@gala.games>
2025-02-05 14:34:32 -0600

    Lint fix

--- test/profile.e2e-spec.ts
+++ test/profile.e2e-spec.ts
@@ -5,32 +5,13 @@ import { addDefaultMocks } from './test-utils';
 import { GiveawayModule } from '../src/giveway-module/giveaway.module';
 import { Web3Module } from '../src/web3-module/web3.module';
 import { MONGO_CLIENT_PROVIDER } from './mocks/mongo.providers';
-import request from 'supertest';
-import { ProfileService } from '../src/profile-module/profile.service';
-import { SigningClient, WalletUtils } from '@gala-chain/connect';
-import { APP_SECRETS } from '../src/secrets/secrets.module';
-import { MockGalachainApi } from './mocks/mock-galachain.api';
-import { GalachainApi } from '../src/web3-module/galachain.api';
-import { GALA_TOKEN } from '../src/constant';
-import { GivewayScheduler } from '../src/giveway-module/giveway-scheduler.service';
-import {
-  GiveawayDocument,
-  GiveawayStatus,
-} from '../src/schemas/giveaway.schema';
-import { Model } from 'mongoose';
-import { getModelToken } from '@nestjs/mongoose';
-import { GiveawayTokenType } from '../src/dtos/giveaway.dto';
 
 jest.setTimeout(50000);
+
+//TODO
 describe('Profile related functions', () => {
   let app: INestApplication;
   let memoryServer: MongoMemoryServer;
-  let profileService: ProfileService;
-  let mockGalachainApi: MockGalachainApi;
-  let giveawayScheduler: GivewayScheduler;
-  let galachainApi: GalachainApi;
-  let giveawayModel: Model<GiveawayDocument>;
-  let secrets: Record<string, any>;
 
   beforeEach(async () => {
     const dbPlaceHolder: Provider = {
@@ -49,14 +30,6 @@ describe('Profile related functions', () => {
     app = compiledFixture.createNestApplication();
     await app.init();
     memoryServer = app.get<MongoMemoryServer>(MONGO_CLIENT_PROVIDER);
-    profileService = await app.resolve<ProfileService>(ProfileService);
-    mockGalachainApi = await app.resolve<MockGalachainApi>(GalachainApi);
-    secrets = await app.resolve<Record<string, any>>(APP_SECRETS);
-    giveawayScheduler = await app.resolve<GivewayScheduler>(GivewayScheduler);
-    galachainApi = await app.resolve<GalachainApi>(GalachainApi);
-    giveawayModel = await app.resolve<Model<GiveawayDocument>>(
-      getModelToken('Giveaway'),
-    );
   });
 
   it('should be defined', () => {
